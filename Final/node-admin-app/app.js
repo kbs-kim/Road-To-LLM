756695e4e5bae12c4c06b75357ffd7b7c95004b2
BUMSU Kim <kbsbmori3@gmail.com>
2024-08-05 21:53:23 +0900

    changed app.js for uploading to Naver Cloud

--- Final/node-admin-app/app.js
+++ Final/node-admin-app/app.js
@@ -1,24 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var expressLayouts = require('express-ejs-layouts');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var expressLayouts = require("express-ejs-layouts");
 
 //환경설정파일 구성하기
-require('dotenv').config();
+require("dotenv").config();
 
+//ORM DB연결객체 sequelize 참조하기
+var sequelize = require("./models/index.js").sequelize;
 
-//ORM DB연결객체 sequelize 참조하기 
-var sequelize = require('./models/index.js').sequelize;
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var adminRouter = require('./routes/admin');
+var adminRouter = require("./routes/admin");
 // var memberRouter = require('./routes/member');
 // var channelRouter = require('./routes/channel');
-var articleRouter = require('./routes/article');
+var articleRouter = require("./routes/article");
 // var messageRouter = require('./routes/message');
 
 var app = express();
@@ -27,45 +26,46 @@ var app = express();
 sequelize.sync();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 //레이아웃 설정
-app.set('layout', 'layout');
+app.set("layout", "layout");
 app.set("layout extractScripts", true);
 app.set("layout extractStyles", true);
 app.set("layout extractMetas", true);
 app.use(expressLayouts);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-app.use('/admin', adminRouter);
+app.use("/admin", adminRouter);
 // app.use('/member', memberRouter);
 // app.use('/channel', channelRouter);
-app.use('/article', articleRouter);
+app.use("/article", articleRouter);
 // app.use('/message', messageRouter);
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-module.exports = app;
+app.set("port", process.env.PORT || 5001);
+//노드앱이 작동되는 서버 객체 생성
+var server = app.listen(app.get("port"), function () {});
--- Final/node-chat-app/app.js
+++ Final/node-chat-app/app.js
@@ -74,5 +74,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
