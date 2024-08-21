81e287e770372346afe12f1a7b354a25c1403a3a
BUMSU Kim <kbsbmori3@gmail.com>
2024-08-21 11:32:52 +0900

    node-final-back-app

--- Front-end/front-final/next-platform-app/pages/chat/index.tsx
+++ Front-end/front-final/next-platform-app/pages/chat/index.tsx
@@ -1,7 +1,45 @@
+//사용자간 기초 채팅기능구현 컴포넌트
+import { useState } from 'react';
+
+import { IMessage } from '@/interfaces/message';
+
 const Chat = () => {
-    return (
-        
- <div className="flex h-screen antialiased text-gray-800 mt-14 pb-10">
+  //현재 사용자 고유번호 상태값 정의
+  const [memberId, setMemberId] = useState<number>(1);
+
+  //채팅 메시지 입력 요소 바인딩 텍스트 상태값 정의
+  const [message, setMessage] = useState<string>('');
+
+  //채팅 메시지 목록(채팅이력정보) 상태값 정의하기
+  const [messageList, setMessageList] = useState<IMessage[]>([
+    {
+      member_id: 1,
+      name: '김철수',
+      profile: 'http://localhost:5000/img/user1.png',
+      message: '안녕하세요',
+      send_date: '2021-09-01 10:00:00',
+    },
+    {
+      member_id: 2,
+      name: '강재명',
+      profile: 'http://localhost:5000/img/user2.png',
+      message: '비가오네요',
+      send_date: '2021-09-01 11:00:00',
+    },
+    {
+      member_id: 3,
+      name: '이영희',
+      profile: 'http://localhost:5000/img/user3.png',
+      message: '철수야 놀자!!!!',
+      send_date: '2021-09-01 12:00:00',
+    },
+  ]);
+
+  //채팅 메시지 전송 이벤트 처리함수
+  const sendMessage = () => {};
+
+  return (
+    <div className="flex h-screen antialiased text-gray-800 mt-14 pb-10">
       <div className="flex flex-row h-full w-full overflow-x-hidden">
         <div className="flex flex-col flex-auto h-full p-6">
           <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
@@ -9,8 +47,46 @@ const Chat = () => {
             <div className="flex flex-col h-full overflow-x-auto mb-4">
               <div className="flex flex-col h-full">
                 <div className="grid grid-cols-12 gap-y-2">
+                  {messageList.map((msg, index) =>
+                    msg.member_id === memberId ? (
+                      <div
+                        key={index}
+                        className="col-start-6 col-end-13 p-3 rounded-lg"
+                      >
+                        <div className="flex items-center justify-start flex-row-reverse">
+                          <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                            A
+                          </div>
+                          <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                            <div>{msg.message}</div>
+                            
+                            <div className ="absolute w-[200px] text-right text-xs bottom-0 right-0 -mb-5 text-gray-500"> 
+                            {msg.name}{msg.send_date} 
+                            </div>
+                            </div>
+
+                          
+                        </div>
+                      </div>
+                    ) : (
+                      <div
+                        key={index}
+                        className="col-start-1 col-end-8 p-3 rounded-lg"
+                      >
+                        <div className="flex flex-row items-center">
+                          <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                            A
+                          </div>
+                          <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                            <div>{msg.message}</div>
+                          </div>
+                        </div>
+                      </div>
+                    ),
+                  )}
+
                   {/* 왼쪾 다른 사용자 메시지 출력 영역 */}
-                  <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                  {/* <div className="col-start-1 col-end-8 p-3 rounded-lg">
                     <div className="flex flex-row items-center">
                       <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
                         A
@@ -19,10 +95,10 @@ const Chat = () => {
                         <div>Hey How are you today?</div>
                       </div>
                     </div>
-                  </div>
+                  </div> */}
 
                   {/* 오른쪽 본인 메시지 출력영역 */}
-                  <div className="col-start-6 col-end-13 p-3 rounded-lg">
+                  {/* <div className="col-start-6 col-end-13 p-3 rounded-lg">
                     <div className="flex items-center justify-start flex-row-reverse">
                       <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
                         A
@@ -33,7 +109,7 @@ const Chat = () => {
                         </div>
                       </div>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
@@ -60,6 +136,11 @@ const Chat = () => {
                 <div className="relative w-full">
                   <input
                     type="text"
+                    name={message}
+                    value={message}
+                    onChange={e => {
+                      setMessage(e.target.value);
+                    }}
                     className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
                   />
                 </div>
@@ -67,7 +148,11 @@ const Chat = () => {
 
               {/* 메시지 전송버튼 영역 */}
               <div className="ml-4">
-                <button className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+                <button
+                  type="button"
+                  onClick={sendMessage}
+                  className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
+                >
                   <span>Send</span>
                   <span className="ml-2">
                     <svg
@@ -87,7 +172,7 @@ const Chat = () => {
         </div>
       </div>
     </div>
-    );
-}
+  );
+};
 
 export default Chat;
